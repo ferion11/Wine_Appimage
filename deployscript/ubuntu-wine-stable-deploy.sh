f8553c884095da87ec34f05c0319a390d0257e0e
Taqi Raza <mttalpur@gmail.com>
2019-03-06 19:51:00 +0500

    fix wine stable version number

--- deployscript/ubuntu-wine-stable-deploy.sh
+++ deployscript/ubuntu-wine-stable-deploy.sh
@@ -5,11 +5,11 @@ apt update
 apt install -y aptitude wget file bzip2
 
 # Get Wine
-wget -nv -c https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-stable_4.3~bionic_i386.deb
-wget -nv -c https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-stable-i386_4.3~bionic_i386.deb
+wget -nv -c https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-stable_4.0~bionic_i386.deb
+wget -nv -c https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-stable-i386_4.0~bionic_i386.deb
 
-dpkg -x wine-stable_4.3~bionic_i386.deb wineversion/
-dpkg -x wine-stable-i386_4.3~bionic_i386.deb wineversion/
+dpkg -x wine-stable_4.0~bionic_i386.deb wineversion/
+dpkg -x wine-stable-i386_4.0~bionic_i386.deb wineversion/
 
 cp -r "wineversion/opt/"* "wineversion"
 rm -r "wineversion/opt"
