484ef79b5228b97892e3a8d4eee43b4bfa8d0b05
Taqi Raza <mttalpur@gmail.com>
2019-03-08 01:57:11 +0500

    enable debian builds & simplify appimage name show only os codename

--- deployscript/buster-wine-staging-deploy.sh
+++ deployscript/buster-wine-staging-deploy.sh
@@ -5,11 +5,11 @@ apt update
 apt install -y aptitude wget file bzip2
 
 # Get Wine
-wget -nv -c https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-staging_4.3~bionic_i386.deb
-wget -nv -c https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-staging-i386_4.3~bionic_i386.deb
+wget -nv -c https://dl.winehq.org/wine-builds/debian/dists/buster/main/binary-i386/wine-staging_4.3~buster_i386.deb
+wget -nv -c https://dl.winehq.org/wine-builds/debian/dists/buster/main/binary-i386/wine-staging-i386_4.3~buster_i386.deb
 
-dpkg -x wine-staging_4.3~bionic_i386.deb wineversion/
-dpkg -x wine-staging-i386_4.3~bionic_i386.deb wineversion/
+dpkg -x wine-staging_4.3~buster_i386.deb wineversion/
+dpkg -x wine-staging-i386_4.3~buster_i386.deb wineversion/
 
 cp -r "wineversion/opt/"* "wineversion"
 rm -r "wineversion/opt"
@@ -19,7 +19,6 @@ wineworkdir=(wineversion/*)
 cd $wineworkdir
 
 # Add a dependency library, such as freetype font library
-mkdir bin
 wget -nv -c https://github.com/Hackerl/Wine_Appimage/releases/download/v0.9/libhookexecv.so -O bin/libhookexecv.so
 wget -nv -c https://github.com/Hackerl/Wine_Appimage/releases/download/v0.9/wine-preloader_hook -O bin/wine-preloader_hook
 
@@ -28,7 +27,7 @@ chmod +x bin/wine-preloader_hook
 pkgcachedir='/tmp/.winedeploycache'
 mkdir -p $pkgcachedir
 
-aptitude -y -d -o dir::cache::archives="$pkgcachedir" install libwine:i386 libva2:i386 libva-drm2:i386 libva-x11-2:i386 libvulkan1:i386 winbind:i386
+aptitude -y -d -o dir::cache::archives="$pkgcachedir" install libwine:i386 libva2:i386 libva-drm2:i386 libva-x11-2:i386 libvulkan1:i386
 
 find $pkgcachedir -name '*deb' ! -name 'libwine*' -exec dpkg -x {} . \;
 
@@ -47,4 +46,4 @@ cp resource/* $wineworkdir
 
 ./appimagetool.AppImage --appimage-extract
 
-export ARCH=x86_64; squashfs-root/AppRun $wineworkdir
+export ARCH=x86_64; squashfs-root/AppRun -v $wineworkdir -u 'gh-releases-zsync|mmtrt|Wine_Appimage|continuous|Wine-x86_64.AppImage.zsync'
