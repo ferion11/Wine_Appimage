cb233c75f0908ad1cb8d2f406010089ad81ac120
Taqi Raza <mttalpur@gmail.com>
2019-03-08 10:06:44 +0500

    test travis build envars

--- deployscript/bionic-wine-staging-deploy.sh
+++ deployscript/bionic-wine-staging-deploy.sh
@@ -5,11 +5,11 @@ apt update
 apt install -y aptitude wget file bzip2
 
 # Get Wine
-wget -nv -c https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-staging_4.3~bionic_i386.deb
-wget -nv -c https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-staging-i386_4.3~bionic_i386.deb
+wget -nv -c https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-staging_${WSTG_VER}~bionic_i386.deb
+wget -nv -c https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-staging-i386_${WSTG_VER}~bionic_i386.deb
 
-dpkg -x wine-staging_4.3~bionic_i386.deb wineversion/
-dpkg -x wine-staging-i386_4.3~bionic_i386.deb wineversion/
+dpkg -x wine-staging_${WSTG_VER}~bionic_i386.deb wineversion/
+dpkg -x wine-staging-i386_${WSTG_VER}~bionic_i386.deb wineversion/
 
 cp -r "wineversion/opt/"* "wineversion"
 rm -r "wineversion/opt"
