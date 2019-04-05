bd84999aa8af51c4a277ee77e3ef12ad25b4fd5e
Taqi Raza <mttalpur@gmail.com>
2019-04-05 12:31:35 +0500

    update wine appimage naming

--- deployscript/bionic-wine-devel-deploy.sh
+++ deployscript/bionic-wine-devel-deploy.sh
@@ -46,4 +46,4 @@ cp resource/* $wineworkdir
 
 ./appimagetool.AppImage --appimage-extract
 
-export ARCH=x86_64; squashfs-root/AppRun -v $wineworkdir -u 'gh-releases-zsync|mmtrt|Wine_Appimage|continuous|Wine-devel*bionic.AppImage.zsync' Wine-devel-i386-${ARCH}-bionic.AppImage
+export ARCH=x86_64; squashfs-root/AppRun -v $wineworkdir -u 'gh-releases-zsync|mmtrt|Wine_Appimage|continuous|wine-devel*bionic.AppImage.zsync' wine-devel-i386_${ARCH}~bionic.AppImage
