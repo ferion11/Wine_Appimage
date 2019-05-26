199340e75bf6313a92fd99d622ab0bba5151629c
Taqi Raza <mttalpur@gmail.com>
2019-05-26 16:30:59 +0500

    bump wine-dev version

--- deployscript/bionic-wine-staging-deploy.sh
+++ deployscript/bionic-wine-staging-deploy.sh
@@ -5,11 +5,11 @@ apt update
 apt install -y aptitude wget file bzip2
 
 # Get Wine
-wget -nv -c https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-staging_4.8~bionic_i386.deb
-wget -nv -c https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-staging-i386_4.8~bionic_i386.deb
+wget -nv -c https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-staging_4.9~bionic_i386.deb
+wget -nv -c https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-staging-i386_4.9~bionic_i386.deb
 
-dpkg -x wine-staging_4.8~bionic_i386.deb wineversion/
-dpkg -x wine-staging-i386_4.8~bionic_i386.deb wineversion/
+dpkg -x wine-staging_4.9~bionic_i386.deb wineversion/
+dpkg -x wine-staging-i386_4.9~bionic_i386.deb wineversion/
 
 cp -r "wineversion/opt/"* "wineversion"
 rm -r "wineversion/opt"
