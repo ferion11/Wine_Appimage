b45a76edcced607a63ac5fad96cc988aa2fbea7b
DanielDevBR <dados.linux@gmail.com>
2019-07-12 19:07:22 -0300

    preparation for full version

--- deployscript/bionic-wine-stable-deploy.sh
+++ deployscript/bionic-wine-stable-deploy.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 # Pre install
 dpkg --add-architecture i386
+
+# Download and add the repository key:
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+apt-key add winehq.key
+
+# Add repository:
+apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
+
 apt update
 apt install -y aptitude wget file bzip2 gcc-multilib
 
 # Purge PulseAudio
-apt purge -y pulseaudio pulseaudio-utils gstreamer0.10-pulseaudio libpulse-browse0 paman pavumeter pavucontrol
+apt purge -y pulseaudio pulseaudio-utils libpulse-browse0 paman pavumeter pavucontrol
 rm /etc/asound.conf
 
-# Get Wine
-wget -nv -c https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-stable_4.0.1~bionic_i386.deb
-wget -nv -c https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-stable-i386_4.0.1~bionic_i386.deb
+## Get Wine
+#wget -nv -c https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-stable_4.0.1~bionic_i386.deb
+#wget -nv -c https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-i386/wine-stable-i386_4.0.1~bionic_i386.deb
 #wget -nv -c https://dl.winehq.org/wine-builds/ubuntu/dists/bionic/main/binary-amd64/wine-stable-amd64_4.0.1~bionic_amd64.deb
 
-dpkg -x wine-stable_4.0.1~bionic_i386.deb wineversion/
-dpkg -x wine-stable-i386_4.0.1~bionic_i386.deb wineversion/
+#dpkg -x wine-stable_4.0.1~bionic_i386.deb wineversion/
+#dpkg -x wine-stable-i386_4.0.1~bionic_i386.deb wineversion/
 #dpkg -x wine-stable-amd64_4.0.1~bionic_amd64.deb wineversion/
+mkdir wineversion
 
-cp -r "wineversion/opt/"* "wineversion"
-rm -r "wineversion/opt"
-rm -rf "wineversion/usr"
+#cp -r "wineversion/opt/"* "wineversion"
+#rm -r "wineversion/opt"
+#rm -rf "wineversion/usr"
 
 # compile & strip libhookexecv wine-preloader_hook
 gcc -shared -fPIC -m32 -ldl src/libhookexecv.c -o src/libhookexecv.so
@@ -33,21 +42,21 @@ cd $wineworkdir
 pkgcachedir='/tmp/.winedeploycache'
 mkdir -p $pkgcachedir
 
-aptitude -y -d -o dir::cache::archives="$pkgcachedir" install libalsaplayer0:i386 libwine:i386 libva2:i386 libva-drm2:i386 libva-x11-2:i386 libvulkan1:i386 libavcodec57:i386
+aptitude -y -d -o dir::cache::archives="$pkgcachedir" install winehq-stable libalsaplayer0 libalsaplayer0:i386 libwine:i386 libva2:i386 libva-drm2:i386 libva-x11-2:i386 libvulkan1:i386 libavcodec57:i386
 
 find $pkgcachedir -name '*deb' ! -name 'libwine*' -exec dpkg -x {} . \;
 
-rm -rf $pkgcachedir ; rm -rf lib/x86_64-linux-gnu ; rm -rf usr/lib/x86_64-linux-gnu ; rm -rf share/man ; rm -rf usr/share/doc ; rm -rf usr/share/lintian ; rm -rf var ; rm -rf sbin ; rm -rf usr/share/man ; rm -rf usr/share/mime ; rm -rf usr/share/pkgconfig ; rm -rf usr/share/wine
+rm -rf $pkgcachedir ; rm -rf share/man ; rm -rf usr/share/doc ; rm -rf usr/share/lintian ; rm -rf var ; rm -rf sbin ; rm -rf usr/share/man ; rm -rf usr/share/mime ; rm -rf usr/share/pkgconfig ; rm -rf usr/share/wine
 
-# Make absolutely sure it will not load stuff from /lib or /usr
-sed -i -e 's|/usr|/xxx|g' lib/ld-linux.so.2
-sed -i -e 's|/usr/lib|/ooo/ooo|g' lib/ld-linux.so.2
+## Make absolutely sure it will not load stuff from /lib or /usr
+#sed -i -e 's|/usr|/xxx|g' lib/ld-linux.so.2
+#sed -i -e 's|/usr/lib|/ooo/ooo|g' lib/ld-linux.so.2
 
-# Remove duplicate (why is it there?)
-rm -f lib/i386-linux-gnu/ld-*.so
+## Remove duplicate (why is it there?)
+#rm -f lib/i386-linux-gnu/ld-*.so
 
-# Disable winemenubuilder
-sed -i 's/winemenubuilder.exe -a -r/winemenubuilder.exe -r/g' share/wine/wine.inf
+## Disable winemenubuilder
+#sed -i 's/winemenubuilder.exe -a -r/winemenubuilder.exe -r/g' share/wine/wine.inf
 
 # appimage
 cd -
