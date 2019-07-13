b3d00b9ed29bcc4f584fbe6b17bd02725a5fcb26
DanielDevBR <dados.linux@gmail.com>
2019-07-13 10:23:53 -0300

    bugfix, refactory, disable pulseaudio, and temporary disable 64bit

--- deployscript/archlinux-wine-deploy.sh
+++ deployscript/archlinux-wine-deploy.sh
@@ -6,6 +6,7 @@ pacman -Syy
 #pacman -S --noconfirm wget file pacman-contrib tar grep gcc lib32-gcc-libs
 pacman -S --noconfirm wget file pacman-contrib tar grep
 
+#===========================================================================================
 # Get Wine
 #wget -nv -c https://www.playonlinux.com/wine/binaries/phoenicis/upstream-linux-amd64/PlayOnLinux-wine-4.10-upstream-linux-amd64.tar.gz
 mkdir wineversion
@@ -22,6 +23,7 @@ mv wine-preloader_hook src/
 #strip src/libhookexecv.so src/wine-preloader_hook
 chmod +x src/wine-preloader_hook
 
+#===========================================================================================
 wineworkdir=(wineversion)
 cd $wineworkdir
 
@@ -33,8 +35,8 @@ mkdir cache
 pacman -Scc --noconfirm
 pacman -Syw --noconfirm --cachedir cache lib32-alsa-lib lib32-alsa-plugins lib32-faudio lib32-fontconfig lib32-freetype2 lib32-gcc-libs lib32-gettext lib32-giflib lib32-glu lib32-gnutls lib32-gst-plugins-base-libs lib32-lcms2 lib32-libjpeg-turbo lib32-libldap lib32-libpcap lib32-libpng lib32-libsm lib32-libxcomposite lib32-libxcursor lib32-libxdamage lib32-libxi lib32-libxml2 lib32-libxmu lib32-libxrandr lib32-libxslt lib32-libxxf86vm lib32-mesa lib32-mesa-libgl lib32-mpg123 lib32-ncurses lib32-openal lib32-sdl2 lib32-v4l-utils lib32-libdrm lib32-libva $dependencys
 
-## Remove non lib32 pkgs before extracting
-#find ./cache -type f ! -name "lib32*" -exec rm {} \;
+# Remove non lib32 pkgs before extracting
+find ./cache -type f ! -name "lib32*" -exec rm {} \;
 
 find ./cache -name '*tar.xz' -exec tar --warning=no-unknown-keyword -xJf {} \;
 
@@ -43,6 +45,7 @@ rm -rf cache; rm -rf include; rm usr/lib32/{*.a,*.o}; rm -rf usr/lib32/pkgconfig
 rm -rf boot; rm -rf dev; rm -rf home; rm -rf mnt; rm -rf opt; rm -rf proc; rm -rf root; rm sbin; rm -rf srv; rm -rf sys; rm -rf tmp; rm -rf var
 rm -rf usr/src; rm -rf usr/share; rm usr/sbin; rm -rf usr/local; rm usr/lib/{*.a,*.o}
 
+#===========================================================================================
 # fix broken link libglx_indirect and others
 rm usr/lib32/libGLX_indirect.so.0
 ln -s libGLX_mesa.so.0 libGLX_indirect.so.0
@@ -56,10 +59,14 @@ rm usr/lib/libkeyutils.so
 ln -s libkeyutils.so.1 libkeyutils.so
 mv libkeyutils.so usr/lib
 
+#===========================================================================================
+# Disable PulseAudio
+rm etc/asound.conf; rm -rf etc/modprobe.d/alsa.conf; rm -rf etc/pulse
 
 ## Disable winemenubuilder
 #sed -i 's/winemenubuilder.exe -a -r/winemenubuilder.exe -r/g' share/wine/wine.inf
 
+#===========================================================================================
 # appimage
 cd ..
 
