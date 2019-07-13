d1e6c17b870e03ba295343074d41a366d2dd8571
DanielDevBR <dados.linux@gmail.com>
2019-07-12 21:09:34 -0300

    trying more simple

--- deployscript/bionic-wine-stable-deploy.sh
+++ deployscript/bionic-wine-stable-deploy.sh
@@ -16,9 +16,9 @@ dpkg -x wine-stable_4.0.1~bionic_amd64.deb wineversion/
 dpkg -x wine-stable-amd64_4.0.1~bionic_amd64.deb wineversion/
 dpkg -x wine-stable-i386_4.0.1~bionic_i386.deb wineversion/
 
-#cp -r "wineversion/opt/"* "wineversion"
-#rm -r "wineversion/opt"
-#rm -rf "wineversion/usr"
+mv "wineversion/opt/*" "wineversion"
+rm -r "wineversion/opt"
+rm -rf "wineversion/usr"
 
 # compile & strip libhookexecv wine-preloader_hook
 gcc -shared -fPIC -m32 -ldl src/libhookexecv.c -o src/libhookexecv.so
@@ -32,9 +32,9 @@ cd $wineworkdir
 pkgcachedir='/tmp/.winedeploycache'
 mkdir -p $pkgcachedir
 
-aptitude -y -r -d -o dir::cache::archives="$pkgcachedir" install winehq-stable libalsaplayer0 libalsaplayer0:i386
+aptitude -y -d -o dir::cache::archives="$pkgcachedir" install libwine libwine:i386 libva2:i386 libva-drm2:i386 libva-x11-2:i386 libavcodec57:i386
 
-find $pkgcachedir -name '*deb' ! -name 'pulse*' -exec dpkg -x {} . \;
+find $pkgcachedir -name '*deb' ! -name 'libwine*' -exec dpkg -x {} . \;
 
 rm -rf $pkgcachedir ; rm -rf share/man ; rm -rf usr/share/doc ; rm -rf usr/share/lintian ; rm -rf var ; rm -rf sbin ; rm -rf usr/share/man ; rm -rf usr/share/mime ; rm -rf usr/share/pkgconfig ; rm -rf usr/share/wine
 
