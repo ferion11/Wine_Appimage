4cb1f3ffe35548deb6ae5cbf581327810edd8353
DanielDevBR <dados.linux@gmail.com>
2020-08-23 22:29:57 -0300

    re-making v5.11

--- deployscript/archlinux-wine-deploy.sh
+++ deployscript/archlinux-wine-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-P_URL="https://www.playonlinux.com/wine/binaries/phoenicis/staging-linux-x86/PlayOnLinux-wine-4.21-staging-linux-x86.tar.gz"
+P_URL="https://www.playonlinux.com/wine/binaries/phoenicis/staging-linux-x86/PlayOnLinux-wine-5.11-staging-linux-x86.tar.gz"
 P_NAME=$(echo $P_URL | cut -d/ -f4)
 P_MVERSION=$(echo $P_URL | cut -d/ -f7)
 P_FILENAME=$(echo $P_URL | cut -d/ -f8)
